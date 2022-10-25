0bd7fe83d613b406f88f2cc1f6d60a7ed0df4476
e1drah <113189459+e1drah@users.noreply.github.com>
2022-10-25 10:15:43 -0300

    Changed Reload, added reset method, changed HUD

--- Arrays/Program.cs
+++ Arrays/Program.cs
@@ -14,30 +14,38 @@ namespace Arrays
 
         //Arrays
         static string[] weaponName = new string[weaponAmmount];
-        static int[] ammo = new int[weaponAmmount];
+        static int[] ammoCurrent = new int[weaponAmmount];
         static int[] ammoMax = new int[weaponAmmount];
+        static int[] ammoTotal = new int[weaponAmmount];
         static void Main(string[] args)
         {
             //Initialization of weapon names
             weaponName[0] = "Pistol";
-            weaponName[1] = "shotgun";
+            weaponName[1] = "Shotgun";
             weaponName[2] = "spreader";
             weaponName[3] = "laser";
             weaponName[4] = "rocket luancher";
 
-            //Initialization of max ammo amount
-            ammoMax[0] = 6; //Max pistol ammo
-            ammoMax[1] = 2; //Max shotgun ammo
-            ammoMax[2] = 50; //Max spreader ammo
-            ammoMax[3] = 25; //Max laser ammo
-            ammoMax[4] = 10; //Max rocket launcher ammo
+            //Initialization of max ammoCurrent amount
+            ammoMax[0] = 6; //Max pistol ammoCurrent
+            ammoMax[1] = 6; //Max Double barrel ammoCurrent
+            ammoMax[2] = 32; //Max spreader ammoCurrent
+            ammoMax[3] = 25; //Max laser ammoCurrent
+            ammoMax[4] = 1; //Max rocket launcher ammoCurrent
 
-            //Initial ammo ammount
-            ammo[0] = ammoMax[weapon]; //pistol
-            ammo[1] = ammoMax[weapon]; //shotgun
-            ammo[2] = ammoMax[weapon]; //spreader
-            ammo[3] = ammoMax[weapon]; //laser
-            ammo[4] = ammoMax[weapon]; //rocket launcher;
+            //Initial ammoCurrent ammount
+            ammoCurrent[0] = ammoMax[0]; //pistol
+            ammoCurrent[1] = ammoMax[1]; //Double barrel
+            ammoCurrent[2] = ammoMax[2]; //spreader
+            ammoCurrent[3] = ammoMax[3]; //laser
+            ammoCurrent[4] = ammoMax[4]; //rocket launcher;
+
+            // Initial of total ammo
+            ammoTotal[0] = 24;
+            ammoTotal[1] = 24;
+            ammoTotal[2] = 128;
+            ammoTotal[3] = 100;
+            ammoTotal[4] = 4;
 
 
             DebugMenu();
@@ -47,12 +55,13 @@ namespace Arrays
         }
         static void DebugMenu()
         {
-
             HUD();
             Console.WriteLine("Fire = 1");
-            Console.WriteLine("Reload = 2");
-            Console.WriteLine("Preveious weapon = 2");
-            Console.WriteLine("Next weapon = 4");
+            Console.WriteLine("Reload quick = 2");
+            Console.WriteLine("Reload slow = 3");
+            Console.WriteLine("Preveious weapon = 4");
+            Console.WriteLine("Next weapon = 5");
+            Console.WriteLine("Reset = 6");
             Console.WriteLine();
 
             int selectionInput = 0;
@@ -75,21 +84,33 @@ namespace Arrays
                     Fire();
                     break;
                 case 2:
-                    Reload();
+                    Reload(selectionInput);
                     break;
                 case 3:
+                    Reload(selectionInput);
+                    break;
+                case 4:
                     ChangeWeapons(selectionInput);
                     break;
 
-                case 4:
+                case 5:
                     ChangeWeapons(selectionInput);
+                    break;
+                case 6:
+                    Reset();
+                    break;
+                default:
+                    Console.WriteLine();
+                    Console.WriteLine(selectionInput + " is a not a valid option");
+                    Console.WriteLine();
+                    DebugMenu();
                     break;
             }
         }
 
         static void Fire()
         {
-            if (ammo[weapon] == 0)
+            if (ammoCurrent[weapon] == 0)
             {
                 Console.WriteLine();
                 Console.WriteLine("Click");
@@ -100,38 +121,88 @@ namespace Arrays
                 Console.WriteLine();
                 Console.WriteLine("BANG!");
                 Console.WriteLine();
-                ammo[weapon] -= 1;
+                ammoCurrent[weapon] -= 1;
             }
             DebugMenu();
         }
         
-        static void Reload()
+        static void Reload(int slick)
         {
+            int ammoCal;    
+            if (ammoTotal[weapon] <= 0)
+            {
+                Console.WriteLine("No ammo left");
+                DebugMenu();
+            }
+            if (ammoCurrent[weapon] == ammoMax[weapon])
+            {
+                Console.WriteLine("No need to Reload");
+                DebugMenu();
+            }
             Console.WriteLine();
             Console.WriteLine("Reloading...");
             Console.WriteLine();
-            ammo[weapon] = ammoMax[weapon];
+            if (slick == 2)
+            {
+                if (weapon == 1)
+                {
+                    ammoCurrent[weapon] += 1;
+                    ammoTotal[weapon] -= 1;
+                }
+                else
+                {
+                    ammoCurrent[weapon] = ammoMax[weapon];
+                    ammoTotal[weapon] -= ammoMax[weapon];
+                }
+            }
+            if (slick == 3)
+            {
+                Console.WriteLine("Reloading...");
+                Console.WriteLine();
+                ammoCal = (ammoMax[weapon] - ammoCurrent[weapon]);
+                ammoTotal[weapon] -= ammoCal;
+                ammoCurrent[weapon] += ammoCal;
+
+            }
+
+
             DebugMenu();
         }
 
-        static void ChangeWeapons(int Nepre)
+        static void ChangeWeapons(int Neious)
         {
-            if (Nepre == 3)
+            if (Neious == 4)
             {
                 if (weapon == 0) weapon = (weaponAmmount - 1);
                 else weapon -= 1;
             }
-            if (Nepre == 4)
+            if (Neious == 5)
             {
                 weapon += 1;
                 if (weapon > (weaponAmmount - 1)) weapon = 0;
             }
-            ammo[weapon] = ammo[weapon];
+            DebugMenu();
+        }
+        static void Reset()
+        {
+            ammoTotal[0] = 24;
+            ammoTotal[1] = 24;
+            ammoTotal[2] = 128;
+            ammoTotal[3] = 100;
+            ammoTotal[4] = 4;
+
+            ammoCurrent[0] = ammoMax[0];
+            ammoCurrent[1] = ammoMax[1];
+            ammoCurrent[2] = ammoMax[2];
+            ammoCurrent[3] = ammoMax[3];
+            ammoCurrent[4] = ammoMax[4];
+
+            Console.Clear();
             DebugMenu();
         }
         static void HUD()
         {
-            Console.WriteLine("Weapon equiped: " + weaponName[weapon] + " Ammo: " + ammo[weapon] + "/" + ammoMax[weapon]);
+            Console.WriteLine("Weapon equiped: " + weaponName[weapon] + " Ammo: " + ammoCurrent[weapon] + "/" + ammoTotal[weapon]);
             Console.WriteLine();
         }
     }
